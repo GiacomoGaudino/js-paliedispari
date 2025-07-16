2b0ad4a6779dc6c23aa7b017c78945785730f594
GiacomoGaudino <giacomo.gaudino@yahoo.it>
2025-07-16 19:55:12 +0200

    add palindrome phrases

--- palindromi/main.js
+++ palindromi/main.js
@@ -1,21 +1,24 @@
 // Palidroma
 
 // Chiedere all’utente di inserire una parola
-let user_word = prompt("inserisci una parola").toLowerCase()
+let user_word = prompt("inserisci una parola o una frase").toLowerCase();
+user_word_without_space = user_word.replace(/ /g, "");
+console.log(user_word_without_space);
 
 // Creare una funzione per capire se la parola inserita è palindroma
-function reverseWord(user_word) {
+function reverseWord(user_word_without_space) {
     let wordReverse = ""
-    for (let i = user_word.length - 1; i >= 0; i--) {
-        const thisWordReverse = user_word[i];
+    for (let i = user_word_without_space.length - 1; i >= 0; i--) {
+        const thisWordReverse = user_word_without_space[i];
         wordReverse += thisWordReverse;
     }
     return wordReverse;
 }
-const wordReverse = reverseWord(user_word);
-console.log(wordReverse);
+const wordReverse = reverseWord(user_word_without_space);
+const wordReverseWithoutSpace = wordReverse.replace(/ /g, "");
+console.log(wordReverseWithoutSpace);
 
-if (user_word === wordReverse) {
+if (user_word_without_space === wordReverseWithoutSpace) {
     console.log("la parola è palindroma");
     alert("la parola è palindroma");
 } else {
